874af08b83ac499fc7a598b36973f83829cf0caa
yunkehe <yunkehe@163.com>
2017-03-03 19:37:07 +0800

    git add

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ var app = express();
 
 var port = process.env.PORT || 9002;
 
-app.use(express.static(__dirname + 'static'));
+app.use(express.static(__dirname + '/static'));
 app.use(function (req, res) {
 	res.sendfile('./static/index.html');
 });
 
 var io = require('socket.io').listen(app.listen(port));
 
+var messages = [];
+
 io.sockets.on('connection', function(socket){
+	socket.on('getAllMessages', function(){
+		socket.emit('allMessages', messages);
+	});
+
+	socket.on('createMessage', function(message){
+		messages.push(message);
+		io.sockets.emit('messageAdded', message);
+	});
 	socket.emit('connected');
 });
 
